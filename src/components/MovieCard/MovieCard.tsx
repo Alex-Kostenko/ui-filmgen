8946a8719336c3a387c3f9835ce24eb90a8837c8
Misha Kompaniec <mikokompaniec@gmail.com>
2023-01-26 15:37:30 +0200

    made movie card and change different styles

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./MovieCard.css";
+import Tag from "../Tag";
+
+export interface MovieCardProps {
+  title?: string;
+  description?: string;
+  date: string;
+  img: string;
+  onClick?: () => any;
+  className?: string;
+  labels?: Array<string>;
+}
+
+const MovieCard = (props: MovieCardProps) => {
+  return (
+    <div {...props} className={"movie-card-container " + props.className}>
+      <img className="movie-img" src={props.img} alt="img" />
+      <div className="description-container">
+        <div className="description-header">
+          <h1 className="movie-title">{props.title}</h1>
+          <p className="movie-date">release date: {props.date}</p>
+          <div className="movie-tag-container">
+            {props.labels?.map((label) => (
+              <Tag className="movie-tag" label={label} />
+            ))}
+          </div>
+        </div>
+        <p className="movie-text">{props.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
