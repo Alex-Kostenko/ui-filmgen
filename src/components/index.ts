f9a72b69ff00066365110802108efd3b3251b7dd
Misha Kompaniec <mikokompaniec@gmail.com>
2023-01-30 15:32:52 +0200

    change export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Button } from './Button';
 export { default as Tag } from './Tag';
-export { default as SeletctComponent } from './Select';
+export { default as SelectComponent } from './Select';
 export { default as MovieCard } from './MovieCard';
 export { default as DatePicker } from './DatePicker';
 export { default as SliderComponent } from './Slider';
