510f2a79714bd717a3bcfa5ffba1a43c924fe8f2
alex-kostenko <0984181760ss@gmail.com>
2023-01-31 11:09:51 +0200

    commit which cancel rule on current line

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -15,6 +15,7 @@ export const Primary = Template.bind({})
 
 Primary.args = {
   placeholder: 'Select',
+  // eslint-disable-next-line
   onChange: () => console.log('1'),
   options: [
     { value: 'marvel', label: 'Marvel' },
