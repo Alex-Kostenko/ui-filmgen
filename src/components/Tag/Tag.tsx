9202a527bfd42851d0cd64c1bbcaf98517ccf5ec
Misha Kompaniec <mikokompaniec@gmail.com>
2023-01-31 09:55:59 +0200

    change props, style and export

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Tag.css';
 
 export interface TagProps {
-  label?: string;
+  label?: string | string[] | undefined;
   onClick?: () => any;
   className: string;
 }
