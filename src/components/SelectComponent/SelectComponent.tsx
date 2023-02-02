49f9c05a5ecfedb5cf1e69f771663f8eeaf8518f
Misha Kompaniec <mikokompaniec@gmail.com>
2023-02-02 11:33:36 +0200

    test

--- src/components/SelectComponent/SelectComponent.tsx
+++ src/components/SelectComponent/SelectComponent.tsx
@@ -1,33 +1,29 @@
 import React from 'react'
-// import Select from 'react-select'
+import Select from 'react-select'
 import './SelectComponent.css'
 
-// type IOptions = {
-//   value: string
-//   label: string
-// }
+type IOptions = {
+  value: string
+  label: string
+}
 
-// type SelectProps = {
-//   onChange?: () => any
-//   className?: string
-//   [name: string]: any
-//   options?: Array<IOptions>
-//   placeholder?: string
-// }
+type SelectProps = {
+  onChange?: () => any
+  className?: string
+  [name: string]: any
+  options?: Array<IOptions>
+  placeholder?: string
+}
 
-const SelectComponent = () => {
+const SelectComponent = (props: SelectProps) => {
   return (
-    // <Select
-    //   placeholder={props.placeholder}
-    //   classNamePrefix='castom-select'
-    //   {...props}
-    //   className={'mainSelectStyle ' + props.className}
-    //   options={props.options}
-    // />
-    <select name='asd' id='1'>
-      <option value='asd'>asd</option>
-      <option value='qwe'>qwe</option>
-    </select>
+    <Select
+      placeholder={props.placeholder}
+      classNamePrefix='castom-select'
+      {...props}
+      className={'mainSelectStyle ' + props.className}
+      options={props.options}
+    />
   )
 }
 
