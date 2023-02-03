1393312ab78bbaccb93cdeb603c86f06acc88b04
Misha Kompaniec <mikokompaniec@gmail.com>
2023-02-03 13:25:07 +0200

    fic

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,7 @@ import postcss from 'rollup-plugin-postcss'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import { babel } from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 
 import packageJson from './package.json' assert { type: 'json' }
 
@@ -23,20 +24,19 @@ export default [
         sourcemap: true,
       },
     ],
-    resolve: {
-      extensions: ['.js', '.ts', '.jsx', '.tsx'],
-    },
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: './tsconfig.json', exclude: ['**/*.stories.tsx', '**/*.test.tsx'] }),
       postcss(),
+      sourcemaps(),
       babel({
         babelHelpers: 'bundled',
         exclude: './node_modules/**',
       }),
     ],
+    external: ['react', 'react-dom', 'react-select', 'react-slick'],
   },
   {
     input: 'dist/esm/types/index.d.ts',
